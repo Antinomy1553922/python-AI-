1ebfa8fc6ab69d02549e58b6002a27907de05fc9
Antinomy1553922 <148140675+Antinomy1553922@users.noreply.github.com>
2023-12-21 14:40:14 +0000

    updata

--- lesson12_1/main.py
+++ lesson12_1/main.py
@@ -2,6 +2,7 @@ import pyinputplus as pyip
 from tools import getStudents,save_to_csv
 
 
+
 if __name__ == "__main__":
     s_nums:int = pyip.inputInt("請輸入學生的數量(1~50人)：",min=1,max=50)
     students:list[str] = getStudents(s_nums)
