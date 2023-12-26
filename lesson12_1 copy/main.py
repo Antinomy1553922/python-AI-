ecd6ab3cd34c745150e74fa34b304f994d827aa4
Antinomy1553922 <148140675+Antinomy1553922@users.noreply.github.com>
2023-12-26 20:25:04 +0800

    updata

---
+++ lesson12_1 copy/main.py
@@ -0,0 +1,10 @@
+import pyinputplus as pyip
+from tools import getStudents,save_to_csv
+
+
+
+if __name__ == "__main__":
+    s_nums:int = pyip.inputInt("請輸入學生的數量(1~50人)：",min=1,max=50)
+    students:list[str] = getStudents(s_nums)
+    fileName = pyip.inputFilename("請輸入檔案名稱(不需要副檔名稱)：")
+    save_to_csv(students,fileName)
