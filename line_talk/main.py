82d21e8a97e8b61951a2e6f1541ab53085019e3b
Antinomy1553922 <148140675+Antinomy1553922@users.noreply.github.com>
2024-01-30 13:40:03 +0000

    updata

--- line_talk/main.py
+++ line_talk/main.py
@@ -2,19 +2,19 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import os
+import os  #抓系統
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
-handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])  #抓取電腦的環境變數(設在render裡面)
+handler = WebhookHandler(os.environ['CHANNEL_SECRET']) #抓取電腦的環境變數(設在render裡面)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/callback", methods=['POST'])
+@app.route("/callback", methods=['POST'])  #如果網址傳回來是callback，就會執行這裡
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers['X-Line-Signature']  #執行這裡，Line的說明書教的
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
@@ -23,9 +23,10 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)   #訊息來會執行這裡
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    print(event.message.text)
+    message = TextSendMessage(text=event.message.text)  #傳過來的文字
     line_bot_api.reply_message(event.reply_token, message)
 
 
